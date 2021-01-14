eb94779c8047908b8667d7e435f86b34a2809e40
slowpokesheep <hjaltigeir@gmail.com>
2021-01-14 14:56:24 +0000

    fix css for mobile maxwidth 600px

--- src/components/core/Footer/index.js
+++ src/components/core/Footer/index.js
@@ -7,32 +7,36 @@ export default function Footer(props) {
         <footer className="footer">
             <div className="footer__content">
                 <div className="footer__company">
-                    <h3>Fyrirtækið</h3>
-                    <ul>
-                        <li>Kosmos & Kaos</li>
-                        <li>
-                            <a href="https://www.kosmosogkaos.is/">Heimasíða</a>
-                        </li>
-                        <li>
-                            <a href="https://www.kosmosogkaos.is/about-us">Um Kosmos & Kaos</a>
-                        </li>
-                        <li>
-                            <a href="mailto:hello@kosmosogkaos.is">hello@kosmosogkaos.is</a>
-                        </li>
-                    </ul>
+                    <div className="footer__innerContent">
+                        <h3>Fyrirtækið</h3>
+                        <ul>
+                            <li>Kosmos & Kaos</li>
+                            <li>
+                                <a href="https://www.kosmosogkaos.is/">Heimasíða</a>
+                            </li>
+                            <li>
+                                <a href="https://www.kosmosogkaos.is/about-us">Um Kosmos & Kaos</a>
+                            </li>
+                            <li>
+                                <a href="mailto:hello@kosmosogkaos.is">hello@kosmosogkaos.is</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="footer__info">
-                    <h3>Upplýsingar</h3>
-                    <ul>
-                        <li>Hjalti Geir Garðarsson</li>
-                        <li>
-                            <a href="mailto:hjaltigeir@gmail.com">hjaltigeir@gmail.com</a>
-                        </li>
-                        <li>666-0369</li>
-                        <li>
-                            <a href="https://github.com/slowpokesheep">Github</a>
-                        </li>
-                    </ul>
+                    <div className="footer__innerContent">
+                        <h3>Upplýsingar</h3>
+                        <ul>
+                            <li>Hjalti Geir Garðarsson</li>
+                            <li>
+                                <a href="mailto:hjaltigeir@gmail.com">hjaltigeir@gmail.com</a>
+                            </li>
+                            <li>666-0369</li>
+                            <li>
+                                <a href="https://github.com/slowpokesheep">Github</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </footer>
